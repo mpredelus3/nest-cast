6fffa5f08eaa3c8b9f72ae2c8c4939141728291c
Jack <alexisyazzie365@gmail.com>
2024-04-09 20:49:51 -0600

    added psuedocode

---
+++ Assets/js/home.js
@@ -0,0 +1,10 @@
+//variables to store element ids
+
+//function to store chosen state to local data
+
+// function to be called upon by the submit button
+    // take chosen park and use function to store to local data
+
+//upon document startup
+    //add event listener to submit button upon click
+    //take user to landing page
---
+++ Assets/js/landing.js
@@ -0,0 +1,27 @@
+// store variables to reference ids/elements on html page, and api key, api base link(s)
+
+// function to pull local data for chosen state
+
+// function to call park api
+    // get list of national parks in state
+
+// function to append park data to the dom
+    // make elements 
+    // append them to variables holding functions
+
+// function to pull weather data for the day
+    // use api call to get today's weather data for park
+
+// function to pull forecast for (5 days?/a week?)
+    // use api call to get forecast data for park 
+
+// function create elements for daily weather and append to the dom 
+    // make elements
+    // append elements
+
+// function create elements for forecast and append to the dom
+    // make elements
+    // append elements
+
+// upon document startup
+    //create event listener for submit button
