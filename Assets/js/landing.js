4849aba992e6ccf33ffaeff1aa1b84396d7db42d
Laplace <lap2837@outlook.com>
2024-04-11 20:34:01 -0600

    added more perameter to fetch data on the landing page

--- Assets/js/landing.js
+++ Assets/js/landing.js
@@ -1,5 +1,7 @@
 const API_KEY = 'U0SqW35wEiSrMcVOGMHfmtu2p4WMQaJgZZU6rAG1';
+// Fetch park details using park code
 const urlParams = new URLSearchParams(window.location.search);
+// Extract park code from URL parameters
 const parkCode = urlParams.get('parkCode');
 
 // Fetch park details using park code
@@ -11,10 +13,14 @@ fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=${API
         const parkDetailsDiv = document.getElementById("parkDetails");
         const parkDetailsBlock = document.createElement("div");
         parkDetailsBlock.innerHTML = `
-            <h2>${park.fullName}</h2>
-            <p>${park.description}</p>
-            <!-- Add more details as needed -->
-        `;
+                    <h2>${park.fullName}</h2>
+                    <p><strong>Directions Info:</strong> ${park.directionsInfo}</p>
+                    <p><strong>Address:</strong> ${park.addresses[0].line1}, ${park.addresses[0].city}, ${park.addresses[0].stateCode} ${park.addresses[0].postalCode}</p>
+
+                    <p><strong>Latitude:</strong> ${park.latitude}, <strong>Longitude:</strong> ${park.longitude}</p>
+                    <p><strong>Weather Info:</strong> ${park.weatherInfo}</p>
+                    <!-- Add more details as needed -->
+                `;
         parkDetailsDiv.appendChild(parkDetailsBlock);
     })
     .catch(error => {
