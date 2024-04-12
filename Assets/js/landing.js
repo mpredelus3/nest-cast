54b4dcc8a09df16b4ac8796b1c2ccd6321306453
Evandaly27 <evan.daly1127@gmail.com>
2024-04-11 20:36:40 -0600

    added weather data

--- Assets/js/landing.js
+++ Assets/js/landing.js
@@ -1,6 +1,7 @@
 const API_KEY = 'U0SqW35wEiSrMcVOGMHfmtu2p4WMQaJgZZU6rAG1';
 const urlParams = new URLSearchParams(window.location.search);
 const parkCode = urlParams.get('parkCode');
+const weatherApi = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial'
 
 // Fetch park details using park code
 fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=${API_KEY}`)
@@ -16,7 +17,36 @@ fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=${API
             <!-- Add more details as needed -->
         `;
         parkDetailsDiv.appendChild(parkDetailsBlock);
+
+        fetchWeather(park.latitude, park.longitude);
     })
     .catch(error => {
         console.error('Error fetching park data:', error);
     });
+
+    function fetchWeather(lat,lon) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApi}&units=imperial`)
+        .then(response => response.json())
+        .then(weather => {
+            displayWeatherDetails(weather);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error)
+        });
+    }
+
+    function displayWeatherDetails(weather) {
+        const weatherDetailsDiv = document.getElementById("weatherDetails");
+        const weatherDetailsBlock = document.createElement("div");
+        if (weather.main && weather.weather && weather.weather.length > 0) {
+            weatherDetailsBlock.innerHTML = `
+                <h4>Current Weather:</h4>
+                <p>Temperature: ${weather.main.temp} °F</p>
+                <p>Weather: ${weather.weather[0].main} (${weather.weather[0].description})</p>
+            `;
+        } else {
+            weatherDetailsBlock.textContent = "Weather data not available.";
+        }
+    
+    weatherDetailsDiv.appendChild(weatherDetailsBlock);
+    }
