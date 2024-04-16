30614c871eb77aadb93c723d20b2d7df9c5ea375
mpredelus3 <141606919+mpredelus3@users.noreply.github.com>
2024-04-15 19:10:05 -0700

    added modal with state codes for reference

--- Assets/js/home.js
+++ Assets/js/home.js
@@ -66,3 +66,20 @@ document.getElementById("nextPage").addEventListener("click", function() {
   }
 });
 
+const openModalBtn = document.getElementById('openModal');
+const modal = document.getElementById('modal');
+const closeModalBtn = document.getElementsByClassName('close')[0];
+
+openModalBtn.addEventListener('click', function() {
+  modal.style.display = 'block';
+});
+
+closeModalBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
